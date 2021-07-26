30b659635549e36f1171d4fdb15f8605c9a91fd7
Deku <zachdsamson@gmail.com>
2021-07-25 21:15:02 -0700

    initial ~unfinished~ ingredient randomized meal search

--- test.js
+++ test.js
@@ -1,18 +1,3 @@
-// // yelp API test
-    // var settings = {
-    //   "url": "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=restaurants&location=Arizona",
-    //   "method": "GET",
-    //   "timeout": 0,
-    //   "headers": {
-    //     "Authorization": "Bearer joFwy_-VZxRWMjwGDywKD-RdbNNUCz2rKI5TeSYJ9QaoDXUNJAzgP8dE7xjMVgJxDUFokND5MPW8bWN6MfquM1bcm37LotJcGqbp9U6LRVJ82crMw_9b8fEY58_4YHYx"
-    //   },
-    // };
-    
-    // $.ajax(settings).done(function (response) {
-    //   console.log(response);
-    // });
-
-
   // randomly generates a meal based on MEAL NAME
   var randomMealName = "https://themealdb.com/api/json/v1/1/random.php?search.php?s=milk&1";
   // generat meal based on INGREDIENT
@@ -20,21 +5,44 @@
   // generate meal based on ID
   var mealIdSearch = "https://themealdb.com/api/json/v1/1/lookup.php?i=53049"
 
+// mustard only give 6 recipes
+
+//TESTER-------------------------------------------------------------//
+  // fetch("https://themealdb.com/api/json/v1/1/filter.php?i=mustard")
+  // .then(function(response) {
+  //   // request was successful
+  //   if (response.ok) {
+  //     response.json().then(function(data) {
+  //       console.log(data);
+  //       if(!data.meals){
+  //         alert("Didn't find it");
+  //       }
+  //       // console.log(data.meals.length);
+  //       storeRecipes(data);
+  //     });
+  //   } else {
+  //     alert('This food does not exist');
+  //   }
+  // })
+  // .catch(function(error) {
+  //   alert("Unable to connect");
+  // });
+//TESTER END -----------------------------------------------------//
 
 
   // <input> element 
   var ingSearch = document.getElementById("ing-search");
   // <button> element
   var searchBtnEl = document.getElementById("search-btn");
+  // recipe list holder
+  var recipeListEl = document.getElementById("recipeList");
 
   // base URL
   var baseUrl = "https://themealdb.com/api/json/v1/1/";
 
-  // var userIng = "eggs";
-  // var ingredient = "filter.php?i=" + userIng;
-  // var ingUrl = baseUrl + ingredient;
-  // console.log(ingUrl);
-
+// TO UPDATE START
+// use get recipes to collect the total count of meals given a specific ingredient && pass to create index
+// TO UPDATE END
   function getRecipes(event){
     console.log(ingSearch.value);
     var userIng = ingSearch.value;
@@ -47,6 +55,11 @@
       if (response.ok) {
         response.json().then(function(data) {
           console.log(data);
+          if(!data.meals){
+            alert("I didn't find that");
+          }
+          // console.log(data.meals.length);
+          storeRecipes(data);
         });
       } else {
         alert('This food does not exist');
@@ -58,6 +71,39 @@
 
   };
   
+  function createIndex() {
+    var indexSelect = [];
+    for(i=0; i<5; i++){
+      var x = Math.floor(Math.random()*40)+1;
+      indexSelect.push(x);
+    }
+    
+    var y = new Set(indexSelect);
+    console.log(y);
+    if(indexSelect.length > y.size){
+      createIndex();
+    }
+    else{
+    console.log("DONE");
+    console.log(indexSelect);
+    z = indexSelect;
+    }
+    return z;
+  }
+  function storeRecipes(data){
+    var indexSelect = [];
+    indexSelect = createIndex();
+    console.log(indexSelect);
+
+    for(i=0; i < indexSelect.length; i++) {
+      var mealName = data.meals[indexSelect[i]].strMeal;
+      var mealId = data.meals[indexSelect[i]].idMeal;
+      console.log(mealName);
+      console.log(mealId);
+    }
+  };
+
+
 
 
   searchBtnEl.addEventListener("click", getRecipes);
